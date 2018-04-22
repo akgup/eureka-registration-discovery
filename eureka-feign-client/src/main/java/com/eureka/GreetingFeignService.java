63d5909eb8c477e172919db7f55be81480ec5196
akshaybuzzy <akshay@buzzybrains.com>
2018-04-22 22:32:20 +0530

    change greeting service client name

--- eureka-feign-client/src/main/java/com/eureka/GreetingFeignService.java
+++ eureka-feign-client/src/main/java/com/eureka/GreetingFeignService.java
@@ -3,7 +3,7 @@ package com.eureka;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name="spring-cloud-eureka-client", fallback=GreetingServiceFallback.class)
+@FeignClient(name="greeting-cloud-service", fallback=GreetingServiceFallback.class)
 public interface GreetingService {
 
 	   @RequestMapping("/greeting")
